643688dc1a5f9fdfec51e4161aa0483a2d3378a0
Murali Dharan S <89293722+murali2601@users.noreply.github.com>
2025-02-22 22:20:33 +0530

    Sample input comment added

--- LeetCode/Amazon Specific/Search_Suggesstion_System.cpp
+++ LeetCode/Amazon Specific/Search_Suggesstion_System.cpp
@@ -1,7 +1,7 @@
 class Solution {
     public:
         vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-            
+
             string prefix = "";
             vector<vector<string>> result;
             sort(products.begin(),products.end());
@@ -27,3 +27,18 @@ class Solution {
             return result;
         }
     };
+
+
+ /*    Example 1:
+
+Input: products = ["mobile","mouse","moneypot","monitor","mousepad"], searchWord = "mouse"
+Output: [["mobile","moneypot","monitor"],["mobile","moneypot","monitor"],["mouse","mousepad"],["mouse","mousepad"],["mouse","mousepad"]]
+Explanation: products sorted lexicographically = ["mobile","moneypot","monitor","mouse","mousepad"].
+After typing m and mo all products match and we show user ["mobile","moneypot","monitor"].
+After typing mou, mous and mouse the system suggests ["mouse","mousepad"].
+Example 2:
+
+Input: products = ["havana"], searchWord = "havana"
+Output: [["havana"],["havana"],["havana"],["havana"],["havana"],["havana"]]
+Explanation: The only word "havana" will be always suggested while typing the search word.
+  */
